d2d56c499b36d72f6e769474e5d5ef37254f2746
Henry Allen-Tilford <htilford@gmail.com>
2014-08-17 20:34:04 +0000

    sockets

---
+++ dockerfiles/base/proxy.js
@@ -0,0 +1,61 @@
+var http = require('http');
+var fs = require('fs');
+var childProcess = require('child_process');
+var path = '/var/run/proxy';
+var httpProxy = require('http-proxy');
+
+if (fs.existsSync(path)) {
+  fs.unlinkSync(path);
+}
+
+var proxy = httpProxy.createProxyServer({});
+
+var server = http.createServer(function (req, res) {
+  console.log('http', req.headers.host);
+  proxy.web(req, res, {
+    target: {
+      host: 'localhost',
+      port: 80
+    }
+  });
+});
+
+server.on('upgrade', function (req, socket, head) {
+  console.log('ws', req.headers.host);
+  proxy.ws(req, socket, head, {
+    target: {
+      host: 'localhost',
+      port: 80
+    }
+  });
+});
+
+var count = 0;
+(function waitForPort () {
+  console.log(count++);
+  if (started) {
+    return false;
+  }
+  setTimeout(function () {
+    var req = http.get('http://localhost', function (res) {
+      res.pipe(process.stdout);
+      res.on('end', start);
+      res.on('error', waitForPort);
+    });
+    req.on('error', waitForPort);
+  }, 20);
+})();
+
+var started = false;
+function start () {
+  if (started) {
+    return false;
+  }
+  started = true;
+  server.listen(path, function (err) {
+    if (err) throw err;
+    console.log('started');
+  });
+}
+
+childProcess.spawn('node', ['/expose/node_modules/expose-bash-over-websockets/server.js']);
